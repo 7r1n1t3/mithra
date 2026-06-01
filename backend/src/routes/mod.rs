b3514da151dd60a1a701efa75c88e0bf78204bbc
Rayen Daadaa <rayen@hlsec.top>
2026-06-01 17:32:38 +0200

    fix(actix,svelte): let svelte handle get requests outside of api/

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,25 +1,7 @@
-use actix_web::{HttpResponse, Responder, get, web};
+use actix_web::web;
 
 pub mod api;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/api").configure(api::configure));
-    cfg.service(get_register);
-    cfg.service(get_signin);
-    cfg.service(health);
-}
-
-#[get("/register")]
-async fn get_register() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[get("/singin")]
-async fn get_signin() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-#[get("/health")]
-async fn health() -> impl Responder {
-    HttpResponse::Ok().body("ok")
 }
